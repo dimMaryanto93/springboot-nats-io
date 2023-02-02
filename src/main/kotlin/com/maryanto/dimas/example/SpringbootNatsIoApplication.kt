febb5d0ff07d20c06baa1aad694293447aa0d4cd
dimasm93 <software.dimas_m@icloud.com>
2023-02-02 14:33:27 +0700

    init project

---
+++ src/main/kotlin/com/maryanto/dimas/example/SpringbootNatsIoApplication.kt
@@ -0,0 +1,11 @@
+package com.maryanto.dimas.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootNatsIoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootNatsIoApplication>(*args)
+}
